d3749564f4f82741c20e16f4c3f4470e0a4624ad
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-27 12:32:58 -0500

    exercise 6.2

--- src/index.js
+++ src/index.js
@@ -27,17 +27,72 @@ const App = () => {
     });
   };
 
+  const noFeedback = () => {
+    if (store.getState().good === 0
+      && store.getState().ok === 0
+      && store.getState().bad === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  const feedback = () => {
+    const all = store.getState().good + store.getState().ok + store.getState().bad;
+    const average = (store.getState().good - store.getState().bad) / all;
+    const positive = store.getState().good / all * 100;
+
+    if (noFeedback()) {
+      return (
+        <div>
+          No feedback given
+        </div>
+      );
+    }
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{store.getState().good}</td>
+          </tr>
+          <tr>
+            <td>ok</td>
+            <td>{store.getState().ok}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{store.getState().bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  };
+
   return (
-    <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>ok</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={zero}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
+    <>
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={good}>good</button>
+        <button onClick={ok}>ok</button>
+        <button onClick={bad}>bad</button>
+        <button onClick={zero}>reset stats</button>
+      </div>
+      <h2>Statistics</h2>
+      {feedback()}
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -45,5 +100,5 @@ const renderApp = () => {
   root.render(<App />)
 }
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
